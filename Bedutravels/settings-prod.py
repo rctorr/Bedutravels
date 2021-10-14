7b67346efc6302bbbfb883e4a85f388614e73a48
Ricardo Torres <rictor@cuhrt.com>
2021-10-13 20:41:23 -0500

    Cambiando configuración del motor de base de datos a MySQL en producción

--- Bedutravels/settings-prod.py
+++ Bedutravels/settings-prod.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'Bedutravels.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rctorr$Bedutravels',
+        'USER': 'rctorr',
+        'PASSWORD': 'pythonsql',
+        'HOST': 'rctorr.mysql.pythonanywhere-services.com',
     }
 }
 
