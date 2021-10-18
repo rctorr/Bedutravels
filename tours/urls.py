0e877bcfbec4cdf4fd6840511d20c6a9dd06cacb
rctorr <rictor@cuhrt.com>
2021-10-18 09:14:47 -0500

    Se agregó la página para agregar un Tour incluyendo ruta, vista, plantilla en html y estilos. Se gregó también la operación de eliminar un Tour incluyendo ruta, y vista

--- tours/urls.py
+++ tours/urls.py
@@ -6,6 +6,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name="index"),
+    path('tour/agregar/', views.tour_agregar, name="tour_agregar"),
+    path('tour/eliminar/<int:id_tour>/', views.tour_eliminar, name="tour_eliminar"),
     path("login/",
     	auth_views.LoginView.as_view(template_name="registration/login.html"),
     	name="login"),
--- tours/views.py
+++ tours/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 from django.shortcuts import render, redirect
 from .models import Tour, Zona
 
@@ -9,7 +10,7 @@ from rest_framework import viewsets
 
 # Create your views here.
 @login_required()
-def index(request):
+def index(request, msg=""):
    """ Atiende la petición GET / """
    tours = Tour.objects.all()
    es_operador = request.user.groups.filter(name="operador").exists()
@@ -18,8 +19,60 @@ def index(request):
       {
          "tours":tours,
          "es_operador":es_operador,
+         "msg":msg,
       }
    )
+
+@login_required()
+def tour_agregar(request):
+   """ Atiende las peticiones GET y POST /tour/agregar/ """
+
+   zonas = Zona.objects.all()
+   es_operador = request.user.groups.filter(name="operador").exists()
+   if es_operador:
+      if request.method == "POST":
+         nombre_form = request.POST.get("nombre")
+         slug_form = request.POST.get("slug", None)
+         tipoDeTour_form = request.POST.get("tipoDeTour", None)
+         descripcion_form = request.POST.get("descripcion")
+         img_form = request.POST.get("img", None)
+         pais_form = request.POST.get("pais", None)
+         zonaSalida_id_form = request.POST.get("zonaSalida", None)
+         zonaSalida_obj = Zona.objects.get(pk=zonaSalida_id_form)
+         zonaLlegada_id_form = request.POST.get("zonaLlegada", None)
+         zonaLlegada_obj = Zona.objects.get(pk=zonaLlegada_id_form)
+         tour = Tour(
+            nombre=nombre_form,
+            slug=slug_form,
+            tipoDeTour=tipoDeTour_form,
+            descripcion=descripcion_form,
+            img=img_form,
+            pais=pais_form,
+            zonaSalida=zonaSalida_obj,
+            zonaLlegada=zonaLlegada_obj,
+            operador=request.user.username,
+         )
+         tour.save()
+         msg = "El Tour ha sido guardado exitosamente!"
+
+         return index(request, msg)
+
+      return render(request, "tours/tour_agregar.html",
+         {
+            "zonas":zonas,
+         }
+      )
+   else:
+      raise Http404("El Tour no existe")
+
+@login_required()
+def tour_eliminar(request, id_tour):
+   """ Atiende las peticiones GET /tour/eliminar/id_tour """
+   tour_obj = Tour.objects.get(pk=id_tour)
+   tour_obj.delete()
+
+   return redirect("/")
+
 
 # Vistas basadas en clases para Django Rest
 class ZonaViewSet(viewsets.ModelViewSet):
